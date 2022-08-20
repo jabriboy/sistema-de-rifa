babcaecf7e6be9c1c512f3102459ad0d0b3a876f
Gabriel Rocha <jabrirocha@hotmail.com>
2022-08-20 18:17:28 -0300

    criação do KivyApp

--- main.py
+++ main.py
@@ -1,20 +1,30 @@
 from functions import *
+from kivy.app import App
+from kivy.uix.relativelayout import RelativeLayout
+
+class Menu(RelativeLayout):
+    pass
+
+class RifaApp(App):
+    pass
+
 
 # add pessoa num banco de dados
 
-create_person()
+#create_person()
 
 
 # faz a conexão da pessoa com os bilhetes da rifa
 
-select_ticket(group=True)
+#select_ticket(group=True)
 
 
 # atualiza dados dos bilhetes
 
-#update_ticket(5086, group=True)
+#update_ticket(49, group=True)
 
-
+rifa = RifaApp()
+rifa.run()
 
 
 
