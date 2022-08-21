f8d70c5bd8eff576be7099c701fe8bc3047204c8
Gabriel Rocha <jabrirocha@hotmail.com>
2022-08-21 00:26:35 -0300

    adição de novas pessoas no banco de dados

--- main.py
+++ main.py
@@ -1,42 +1,13 @@
 from functions import *
 
 # add pessoa num banco de dados
-
 create_person()
 
 
 # faz a conexão da pessoa com os bilhetes da rifa
-
 select_ticket(group=True)
 
 
 # atualiza dados dos bilhetes
-
 #update_ticket(5086, group=True)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-#print(db_num.get_by_id(25))
-#print(db_pessoa.get_by_id(25))
-#print(db_num.get_by_number(777))
-
-#print(db_num.get_by_number(3363))
-
-#print(count_tickets(pagos=True))
 
